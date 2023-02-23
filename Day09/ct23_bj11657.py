26be17f21ef6b0cc33e0873b119fa86f7aa6db50
Gayeon_Lee913 <gy9411@naver.com>
2023-02-23 17:25:44 +0900

    2월 23일 완료

---
+++ Day09/ct23_bj11657.py
@@ -0,0 +1,33 @@
+# 백준 11657번 문제 - 타임머신
+import sys
+
+N, M = map(int, input().split())
+edges = []
+distance = [sys.maxsize] * (N+1)
+
+for _ in range(M):  #에지 리스트 저장
+    start, end, time = map(int, input().split())
+    edges.append((start, end, time))
+
+# 벨만포드 수행
+distance[1] = 0
+
+for _ in range(N-1):    # 노드개수 -1 까지 반복
+    for start, end, time in edges:
+        if distance[start] != sys.maxsize and distance[end] > distance[start] + time:
+            distance[end] = distance[start] + time
+
+# 음수 사이클
+for start, end, time in edges:
+    if distance[start] != sys.maxsize and distance[end] > distance[start] + time:
+        mCycle = True   # 음수사이클이 존재
+        break   # 음수사이클 있으면 더 이상 진행 할 필요 없음
+
+if mCycle != True:
+    for i in range(2, N+1):
+        if distance[i] != sys.maxsize:
+            print(distance[i])
+        else:   # 도시에 방문 안하는 경우
+            print(-1)
+else:
+    print(-1)
